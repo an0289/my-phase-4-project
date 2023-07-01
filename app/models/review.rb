bd8236d295f4417464133c71b8ddc1c86bab66ce
Alexandra <alexandraneff@live.com>
2023-06-30 22:04:57 -0500

    added has_secure_password

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :user_id
   belongs_to :item_id
+
+  validates :title, presence: true 
+  validates :body, presence: true 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    has_many :items 
+    has_many :reviews, through: :items 
+
+    has_secure_password 
+
+    validates :username, presence: true, uniqueness: true 
 end
